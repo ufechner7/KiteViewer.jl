510fdc7de043b7db395986d92286f1c2c5ad9c3f
Uwe Fechner <fechner@aenarete.eu>
2018-11-25 15:36:27 +0100

    added spheres

--- src/KiteViewer.jl
+++ src/KiteViewer.jl
@@ -5,23 +5,25 @@ module KiteViewer
 using Makie
 using GeometryTypes
 
-export init, show3d
+export rects, spheres, show3d
 
-global scene
+global scene = Scene()
 
-greet() = print("Hello World!")
-
-function init()
-    global scene
+function rects()
     x = Vec3f0(0); baselen = 0.2f0; dirlen = 1f0
     # create an array of differently colored boxes in the direction of the 3 axes
     rectangles = [
         (HyperRectangle(Vec3f0(x), Vec3f0(dirlen, baselen, baselen)), RGBAf0(1,0,0,1)),
         (HyperRectangle(Vec3f0(x), Vec3f0(baselen, dirlen, baselen)), RGBAf0(0,1,0,1)),
-        (HyperRectangle(Vec3f0(x), Vec3f0(baselen, baselen, dirlen)), RGBAf0(0,0,1,1))
+        (HyperRectangle(Vec3f0(x), Vec3f0(baselen, baselen, dirlen)), RGBAf0(0,0,1,1)),
     ]
     meshes = map(GLNormalMesh, rectangles)
-    scene=mesh(merge(meshes))
+    mesh!(merge(meshes))
+    nothing
+end
+
+function spheres()
+    mesh!(Sphere(Point3f0(0), 1f0), color = :blue)
     nothing
 end
 
